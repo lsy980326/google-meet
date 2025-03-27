2c67cf61e75b6883fab836c5d79b6457f21c8d59
lsy980326 <cxv963@gmail.com>
2025-03-27 19:00:38 +0900

    Feat: Socket.IO를 활용한 클라이언트-서버 실시간 연결 구현
    - 클라이언트에서 socket.io.js를 로드하고 io.connect()를 통해 서버에 연결
    - 서버에서는 socket.io 모듈을 이용해 클라이언트 연결 수신 및 socket id 출력
    - 사용자 입력(user_id) 및 URL 쿼리(meetingID)를 통해 접속 유효성 검사 수행
    - 연결 성공 시 "socket connected" 알림창으로 사용자에게 연결 상태 안내
    - 실시간 화상회의 기능 구현을 위한 signaling 기반 구조 초기 세팅

---
+++ public/Assets/js/app.js
@@ -0,0 +1,20 @@
+var MyApp = (function(){
+
+    function init(uid, mid){
+        event_process_for_signaling_server();
+    }
+
+    var socket = null;
+    function event_process_for_signaling_server(){
+        socket = io.connect()
+        socket.on("connect",() =>{
+            alert("socket connected")
+        })
+    }
+
+    return {
+        _init: function(uid, mid){
+            init(uid, mid)
+        }
+    }
+})();
